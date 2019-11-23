c714df6fa8e8be9accd38af1ca83b3c5002757d6
Erica Little <eteney90@gmail.com>
2019-11-23 20:11:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-  puts "Welcome, #{name}. You are #{index+1} in line."
+  puts "Welcome, #{name}. You are #{array.index(name)+1} in line."
 end
